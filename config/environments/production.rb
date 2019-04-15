d846485a1493acd0ff30ebb6e2f99d219426ea2b
Ruel Nopal <radon1284@yahoo.com>
2019-04-16 01:05:59 +0800

    sdsjgdjsgd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,5 +93,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.enable_dependency_loading = true
-  config.autoload_paths << Rails.root.join('lib')`
+  config.autoload_paths << Rails.root.join('lib')
 end
