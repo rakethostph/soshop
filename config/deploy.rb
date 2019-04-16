fd92bf9a0faf223f294f01f1e49f2d501445ecbe
Ruel Nopal <radon1284@yahoo.com>
2019-04-16 23:29:20 +0800

    scgsjcgjs

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,6 +80,14 @@ namespace :deploy do
     end
   end
 
+  namespace :rake do
+    desc "Invoke rake task"
+    task :invoke do
+      run "cd #{deploy_to}/current"
+      run "bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}"
+    end
+  end
+
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
