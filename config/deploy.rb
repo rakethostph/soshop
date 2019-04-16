7bbd89795aeb65380516073e323f5a0f99f25b10
Ruel Nopal <radon1284@yahoo.com>
2019-04-16 21:35:53 +0800

    dsdsgdjsg

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-append :linked_files, "config/master.key"
+set :linked_files, %w{config/master.key}
 
 ## Defaults:
 # set :scm,           :git
