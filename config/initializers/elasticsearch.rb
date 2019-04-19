b7289cdaf91c5394ac5a4e1c1ec0f0f0a8bd92b8
Ruel Nopal <radon1284@yahoo.com>
2019-04-20 04:24:26 +0800

    sdsgjsgsjg

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -6,7 +6,7 @@ config = {
 }
 
 if File.exists?("config/elasticsearch.yml")
-  config.merge!(YAML.load_file("config/elasticsearch.yml")[Rails.env].symbolize_keys)
+  config.merge!(YAML.load_file("config/elasticsearch.yml").symbolize_keys)
 end
 
 Elasticsearch::Model.client = Elasticsearch::Client.new(config)
